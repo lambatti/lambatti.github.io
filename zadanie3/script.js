350e50784c682755c79ce8dcf4801079c6868272
Adam <lambatti@wp.pl>
2022-03-15 15:59:25 +0100

    Added solution filling

--- zadanie3/script.js
+++ zadanie3/script.js
@@ -25,11 +25,54 @@ tiles = document.querySelectorAll(".tile")
 tiles.forEach(tile => {
     tile.addEventListener("click", function (param) {
         addTile(tile)
+        addTileToSolution(tile)
+        console.log(solution)
         checkResult()
+        changeNextElement()
         // usuń eventlistener po kliknięciu
     })
 })
 
+function addTileToSolution(tile) {
+    switch (tile.id) {
+        case 'tile1': {
+            solution[0][0] = nextElement
+            break
+        }
+        case 'tile2': {
+            solution[0][1] = nextElement
+            break
+        }
+        case 'tile3': {
+            solution[0][2] = nextElement
+            break
+        }
+        case 'tile4': {
+            solution[1][0] = nextElement
+            break
+        }
+        case 'tile5': {
+            solution[1][1] = nextElement
+            break
+        }
+        case 'tile6': {
+            solution[1][2] = nextElement
+            break
+        }
+        case 'tile7': {
+            solution[2][0] = nextElement
+            break
+        }
+        case 'tile8': {
+            solution[2][1] = nextElement
+            break
+        }
+        case 'tile9': {
+            solution[2][2] = nextElement
+            break
+        }
+    }
+}
 
 const X = '<p class="tile-X">X</p>'
 const O = '<p class="tile-O">O</p>'
@@ -48,7 +91,6 @@ function addTile(tile) {
     // sprawdź, czy kliknięte pole nie jest już wypełnione
     if (tile.innerHTML === '') {
         tile.innerHTML = getNextElement()
-        changeNextElement()
     }
 
     // jeżeli nie, to dodaj X lub O i uzupełnij macierz rozwiązań
