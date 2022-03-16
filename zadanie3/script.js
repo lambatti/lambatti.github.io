83eab79ea8748ddd24199e3e14d83e0f4adb6061
lambatti <adamgrzegorzgala@gmail.com>
2022-03-16 07:41:28 +0100

    Changed divs to buttons, added winning logic

--- zadanie3/script.js
+++ zadanie3/script.js
@@ -1,24 +1,11 @@
-// const tile1 = document.getElementById("tile1")
-// const tile2 = document.getElementById("tile2")
-// const tile3 = document.getElementById("tile3")
-// const tile4 = document.getElementById("tile4")
-// const tile5 = document.getElementById("tile5")
-// const tile6 = document.getElementById("tile6")
-// const tile7 = document.getElementById("tile7")
-// const tile8 = document.getElementById("tile8")
-// const tile9 = document.getElementById("tile9")
+const divX = '<p class="tile-X pressed">X</p>'
+const divO = '<p class="tile-O pressed">O</p>'
+
+let solution = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+let moves = 0
 
 let nextElement = 'O'
-
-function changeNextElement() {
-    if (nextElement === 'O') {
-        nextElement = 'X'
-    } else {
-        nextElement = 'O'
-    }
-}
-
-solution = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
 tiles = document.querySelectorAll(".tile")
 
@@ -26,78 +13,119 @@ tiles.forEach(tile => {
     tile.addEventListener("click", function (param) {
         addTile(tile)
         addTileToSolution(tile)
-        console.log(solution)
         checkResult()
         changeNextElement()
-        // usuń eventlistener po kliknięciu
+        tile.disabled = true
     })
 })
 
+function addTile(tile) {
+    tile.innerHTML = getNextElementDiv()
+}
+
+function getNextElementDiv() {
+    if (nextElement === 'O') {
+        return divO
+    } else {
+        return divX
+    }
+}
+
 function addTileToSolution(tile) {
+
+    let numberToInsert = getNumberToInsert(nextElement)
+
     switch (tile.id) {
         case 'tile1': {
-            solution[0][0] = nextElement
+            solution[0][0] = numberToInsert
             break
         }
         case 'tile2': {
-            solution[0][1] = nextElement
+            solution[0][1] = numberToInsert
             break
         }
         case 'tile3': {
-            solution[0][2] = nextElement
+            solution[0][2] = numberToInsert
             break
         }
         case 'tile4': {
-            solution[1][0] = nextElement
+            solution[1][0] = numberToInsert
             break
         }
         case 'tile5': {
-            solution[1][1] = nextElement
+            solution[1][1] = numberToInsert
             break
         }
         case 'tile6': {
-            solution[1][2] = nextElement
+            solution[1][2] = numberToInsert
             break
         }
         case 'tile7': {
-            solution[2][0] = nextElement
+            solution[2][0] = numberToInsert
             break
         }
         case 'tile8': {
-            solution[2][1] = nextElement
+            solution[2][1] = numberToInsert
             break
         }
         case 'tile9': {
-            solution[2][2] = nextElement
+            solution[2][2] = numberToInsert
             break
+        }
+    }
+    console.log(solution)
+}
+
+function getNumberToInsert(element) {
+    switch (element) {
+        case 'X': {
+            return -1
+        }
+        case 'O': {
+            return 1
         }
     }
 }
 
-const X = '<p class="tile-X">X</p>'
-const O = '<p class="tile-O">O</p>'
-
-function getNextElement() {
-    if (nextElement === 'O') {
-        return O
-    } else {
-        return X
-    }
-}
-
-function addTile(tile) {
-
-    console.log(tile.innerHTML)
-    // sprawdź, czy kliknięte pole nie jest już wypełnione
-    if (tile.innerHTML === '') {
-        tile.innerHTML = getNextElement()
-    }
-
-    // jeżeli nie, to dodaj X lub O i uzupełnij macierz rozwiązań
-
-
-}
-
 function checkResult() {
-    // sprawdź, czy wygrana
+    moves++
+    if(moves >= 5) {
+       for (let i = 0; i <=2; i++) {
+           rowSum = solution[i][0] + solution[i][1] + solution[i][2]
+           columnSum = solution[0][i] + solution[1][i] + solution[2][i]
+           if (rowSum === 3) {
+               alert('Wygrał O')
+               return
+           } else if (rowSum === -3) {
+               alert('Wygrał X')
+               return
+           } else if (columnSum === 3) {
+               alert('Wygrał O')
+               return
+           } else if (columnSum === -3) {
+               alert('Wygrał X')
+               return
+           }
+       }
+       diagonalSum1 = solution[0][0] + solution[1][1] + solution[2][2]
+       diagonalSum2 = solution[0][2] + solution[1][1] + solution[2][0]
+       if (diagonalSum1 === 3 || diagonalSum2 === 3) {
+           alert('Wygrał O')
+           return
+       } else if (diagonalSum1 === -3 || diagonalSum2 === -3) {
+           alert('Wygrał X')
+           return
+       }
+    }
+    if(moves === 9) {
+        alert('Remis')
+    }
+}
+
+function changeNextElement() {
+    if (nextElement === 'O') {
+        nextElement = 'X'
+    } else {
+        nextElement = 'O'
+    }
 }
